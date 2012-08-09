c5ea1154708acb80cacf29dc6db9faaf6599e900
John McGovern <johnmcg7@gmail.com>
2012-08-09 13:49:02 -0700

    Header for new BNRMapPoint class

---
+++ Whereami/Whereami/BNRMapPoint.m
@@ -0,0 +1,13 @@
+//
+//  BNRMapPoint.m
+//  Whereami
+//
+//  Created by John McGovern on 8/9/12.
+//  Copyright (c) 2012 John McGovern. All rights reserved.
+//
+
+#import "BNRMapPoint.h"
+
+@implementation BNRMapPoint
+
+@end
