4276e38b407d7529133bdd7667d62b3f98dbc89b
John McGovern <johnmcg7@gmail.com>
2012-08-09 14:59:42 -0700

    Custom initializers for BNRMapPoint

--- Whereami/Whereami/BNRMapPoint.m
+++ Whereami/Whereami/BNRMapPoint.m
@@ -10,4 +10,23 @@
 
 @implementation BNRMapPoint
 
+@synthesize coordinate, title;
+
+-(id)initWithCoordinate:(CLLocationCoordinate2D)c title:(NSString *)t
+{
+    self = [super init];
+    if(self)
+    {
+        coordinate = c;
+        [self setTitle:t];
+    }
+    return self;
+}
+
+-(id)init
+{
+    return [self initWithCoordinate:CLLocationCoordinate2DMake(43.07, -89.32) title:@"Hometown"];
+}
+
+
 @end
